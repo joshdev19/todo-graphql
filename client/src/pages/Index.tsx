403c87464ee4c69d45b361dd4740f04d2beb6f6e
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 15:00:44 +0800

    Feat: TodosWrapper was imported

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import TodosWrapper from "../components/TodosWrapper";
 
 const Index = () => {
     return (
         <div className="wrapper">
             <Header/>
+            <TodosWrapper/>
             <Footer/>
         </div>
     )
