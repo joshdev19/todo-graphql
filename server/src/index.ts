4b31524b51721a511c3518f434249549321f990e
joshua@doableservices.com <joshua@doableservices.com>
2024-03-02 11:00:04 +0800

    First commit

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import formData from 'express-form-data';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { graphqlHTTP } from 'express-graphql';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.json());
+app.use(formData.parse());
+app.use(cors());
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: process.env.DEV_STATUS === "development" ? true : false
+// }))
+
+app.listen(PORT, () => console.log(`Server is running at port localhost:${PORT}`));
