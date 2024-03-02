6a2cc035e32ce635d7e9556d39449f5434e7132b
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 15:10:56 +0800

    Feat: set the route for add and update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Index from "./pages/Index"
+import Add from "./pages/Add"
+import Update from "./pages/Update"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Index/>}/>
+      <Route path="/add" element={<Add/>}/>
+      <Route path="/update/:id" element={<Update/>}/>
     </Routes>
   )
 }
