83fe37b4dac70e8ac7c3687fbfbccf7c9d2f7758
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 16:59:35 +0800

    Feat: imported schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import formData from 'express-form-data';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { graphqlHTTP } from 'express-graphql';
-
+import { schema } from '../schemas/schema';
 dotenv.config();
 
 const PORT = process.env.PORT || 3002;
@@ -12,9 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(formData.parse());
 app.use(cors());
-// app.use('/graphql', graphqlHTTP({
-//     schema,
-//     graphiql: process.env.DEV_STATUS === "development" ? true : false
-// }))
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.DEV_STATUS === "development" ? true : false
+}))
 
-app.listen(PORT, () => console.log(`Server is running at port localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running at port http://localhost:${PORT}`));
