61a5b42c6be90e8ba3ac6db17d2b391e1d35dafe
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 16:17:55 +0800

    Feat: Created new fake data

--- client/data.ts
+++ client/data.ts
@@ -2,7 +2,7 @@ export const Data = [
     {
         id: 1,
         title: "title 1",
-        description: "Description 1",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, qui alias earum placeat vitae, eaque totam velit pariatur architecto repellat, quae vel quo laboriosam aspernatur odit veniam. Deserunt, repellat adipisci!",
         done: false
     },
     {
@@ -10,5 +10,11 @@ export const Data = [
         title: "title 2",
         description: "Description 2",
         done: false
-    }
+    },
+    {
+        id: 3,
+        title: "To the supermarketdawdawdaw",
+        description: "I'm going to the super market to buy some dishes that will be use for the dinner night.",
+        done: false
+    },
 ]
