8e3f7cafbde34882f52d8f7b551dc9c5b570ba4b
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 16:16:55 +0800

    Feat: Added edit link and substrings

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -1,16 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TODOTYPES } from "../types/types";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
-const Todos = ( { title, description, done }: TODOTYPES ) => {
+const Todos = ( { id, title, description, done }: TODOTYPES ) => {
 
     return (
         <div className="todos">
+            <Link to={`/update/${id}`} className="edit">
+                <FontAwesomeIcon icon={faEdit}/>
+            </Link>
             <button type="button" className="delete">
                 <FontAwesomeIcon icon={faTrash}/>
             </button>
-            <h3> { title } </h3>
-            <p> { description } </p>
+            <p className="title"> { title.substring(0, 18) }.. </p>
+            <p className="description"> { description.substring(0, 70) } .. </p>
             <div className="mark-wrapper">
                 <p> Mark as done </p>
                 <input type="checkbox" />
