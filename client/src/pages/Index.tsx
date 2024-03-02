c8128bdf3965d43c2742d1d859ce64e1b5771346
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 14:41:18 +0800

    Feat: create Index component

---
+++ client/src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Index = () => {
+    return (
+        <div className="wrapper">
+            <Header/>
+        </div>
+    )
+}
+
+export default Index;
