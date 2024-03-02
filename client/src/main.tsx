ff06a0eb30f9ec4ddbc4e40fa5c25e1185bbb70e
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 11:18:05 +0800

    Feat: Set up the provider and router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom'
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache()
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
 )
