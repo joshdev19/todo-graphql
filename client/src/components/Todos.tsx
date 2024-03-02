bbbf65e47a146f631ac1e0b026a764e9f4c11015
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-02 14:59:45 +0800

    Feat: Made instances

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -1,8 +1,20 @@
-const Todos = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { TODOTYPES } from "../types/types";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const Todos = ( { title, description, done }: TODOTYPES ) => {
 
     return (
         <div className="todos">
-            
+            <button type="button" className="delete">
+                <FontAwesomeIcon icon={faTrash}/>
+            </button>
+            <h3> { title } </h3>
+            <p> { description } </p>
+            <div className="mark-wrapper">
+                <p> Mark as done </p>
+                <input type="checkbox" />
+            </div>
         </div>
     )
 
