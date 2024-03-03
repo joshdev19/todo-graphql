f0d6895ae8509808fa6281dcc02ba5fbab279916
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-03 11:04:43 +0800

    Feat: created new statements for todos

---
+++ client/src/graphql/todos/queries.ts
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const GET_TODOS = gql`
+    query {
+        todos {
+            id
+            title
+            description
+            done
+        }
+    }
+`
--- server/sql-statements/sql.ts
+++ server/sql-statements/sql.ts
@@ -1,3 +1,7 @@
 export const statments = {
-    getTodos: "select * from todos"
+    getTodos: "select * from todos",
+    getTodosByID: "select * from todos where id = ?",
+    createTodo: "insert into ( title, description, done ) values ( ? )",
+    updateTodo: "update todo set title = ?, description = ?, done = ?",
+    deleteTodo: "delete from todos where id = ?"
 }
