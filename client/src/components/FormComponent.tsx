cedde851c9444d2342b81e86d73ba3cf3f866711
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-03 14:07:43 +0800

    Feat: Imported Create todo and made a cache update

--- client/src/components/FormComponent.tsx
+++ client/src/components/FormComponent.tsx
@@ -1,18 +1,56 @@
+import { useState } from "react";
 import { useLocation, useParams } from "react-router-dom"
+import { TODOTYPES } from "../types/types";
+import { CREATE_TODO } from "../graphql/todos/todos_mutations";
+import { useMutation } from "@apollo/client";
+import { GET_TODOS } from "../graphql/todos/todos_queries";
+
 
 const FormComponent = () => {
 
     const location = useLocation();
     const path = location.pathname;
     const { id } = useParams();
+    const [ values, setValues ] = useState<TODOTYPES>({
+        title: "",
+        description: ""
+    })
+    const [createTodo] = useMutation(CREATE_TODO, {
+        variables: {
+            title: values?.title,
+            description: values?.description
+        },
+        update(cache, { data: { createTodo } }) {
+            const { todos } = cache.readQuery({ query: GET_TODOS });
+            cache.writeQuery({
+                query: GET_TODOS,
+                data: { todos: [...todos, createTodo] }
+            })
+        }
+    })
+
+    const controlledInputs = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+        const name: string = e.target.name;
+        const value: string = e.target.value;
+        setValues( ( prev ) => ( { ...prev, [name]: value } ))
+    }
+
+    const submitHandler = async () => {
+        if(path.includes('/add')) {
+            createTodo(values?.title, values?.description)
+        }
+        else {
+            ""
+        }
+    }
 
     return (
         <div className="form-wrapper">
             <form>
                 <h2> { path.includes('/add') ? "Add New Todo " : "Update Todo" } </h2>
-                <input type="text" placeholder="Title..." />
-                <input type="text" placeholder="Description..." />
-                <button type="button">
+                <input type="text" name="title" onChange={controlledInputs} placeholder="Title..." value={values?.title} />
+                <input type="text" name="description" onChange={controlledInputs} placeholder="Description..." value={values?.description} />
+                <button type="button" onClick={submitHandler}>
                     {
                         path.includes('/add') ?
                         "Add" :
