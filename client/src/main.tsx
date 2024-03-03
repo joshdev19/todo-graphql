eef7ac017ce8ed4441c3958aecaf1a1f8d946791
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-03-03 14:27:17 +0800

    Feat: Imported Toastcontainer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,8 @@ import App from './App.tsx'
 import './index.css'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
@@ -13,6 +15,18 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
     <BrowserRouter>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </BrowserRouter>
   </ApolloProvider>,
 )
